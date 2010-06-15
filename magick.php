93c3f81160a03155e812045ae8f019861a97fa0d
U-AD\juewang <juewang@7746JK1.ad.aaas.org>
2010-06-15 14:15:46 -0400

    decided on random sampling to compute center-of-edginess. some debug / benchmarking code left in this version

--- magick.php
+++ magick.php
@@ -6,9 +6,12 @@ define('CACHE_PATH', 'imagecache/');
 define('CONVERT_PATH', 'convert');//'/usr/local/bin/convert';
 define('LOG_PATH', 'log.magick.txt');
 // toggle output of dprint() function
-define('DEBUG', 0);
+define('DEBUG', 1);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+// cropping algorithms
+include_once('functions.php');
 
 // execute the script
 main();
@@ -27,7 +30,7 @@ function main() {
     // extract the commands from the query string
     // eg.: ?resize(....)+flip+blur(...)
     if (isset($_GET['cmd'])) {
-        preg_match_all('/\+*(([a-z]+)(\(([^\)]*)\))?)\+*/', $_GET['cmd'],
+        preg_match_all('/\+*(([a-z\-]+[0-9]*)(\(([^\)]*)\))?)\+*/', $_GET['cmd'],
             $cmds, PREG_SET_ORDER);
     }
     // no commands specified
@@ -144,7 +147,7 @@ function dispatch($image, $cache, $cmds) {
         // $cmd[4] the parameter
 
         // check input
-        if (!preg_match('/^[a-z]+$/',$cmd[2])) {
+        if (!preg_match('/^[a-z\-]+[0-9]*$/',$cmd[2])) {
             die('ERROR: Invalid command.');
         }
         if (!preg_match('/^[a-z0-9\/{}+-<>!@%]+$/',$cmd[4])) {
@@ -170,55 +173,54 @@ function dispatch($image, $cache, $cmds) {
             break;
 
         case 'opticrop':
-            // crops the image to the requested size
-            // chooses the crop with the most edges, or "interestingness"
-            if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
-                die('ERROR: Invalid parameter.');
+            if (function_exists('opticrop')) {
+                // crops the image to the requested size
+                // chooses the crop with the most edges, or "interestingness"
+                if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
+                    die('ERROR: Invalid parameter.');
+                }
+                list($width, $height) = explode('x', $cmd[4]);
+                $result = opticrop($image, $width, $height, $cache);
             }
-            list($width, $height) = explode('x', $cmd[4]);
-            $result = opticrop($image, $width, $height, $cache);
+            else {
+                die('Function does not exist. Check includes.');
+            }
+            break;
+
+         case 'liquid':
+            if (function_exists('opticrop')) {
+                if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
+                    die('ERROR: Invalid parameter.');
+                }
+                list($w, $h) = explode('x', $cmd[4]);
+                $im = new Imagick($image);
+                $im->liquidRescaleImage( $w, $h, 3, 25 );
+                $im->writeImage($cache);
+            }
+            else {
+                die('Function does not exist. Check includes.');
+            }
+            break;
+
+       case 'opticrop2':
+            if (function_exists('opticrop2')) {
+                // refinement of opticrop 1
+                if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
+                    die('ERROR: Invalid parameter.');
+                }
+                list($width, $height) = explode('x', $cmd[4]);
+                $result = opticrop2($image, $width, $height, $cache);
+            }
             break;
 
         case 'part':
-            // crops the image to the requested size
-            if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
-                die('ERROR: Invalid parameter.');
-            }
-
-            list($width, $height) = explode('x', $cmd[4]);
-
-            // get size of the original
-            $imginfo = getimagesize($image);
-            $orig_w = $imginfo[0];
-            $orig_h = $imginfo[1];
-
-            // resize image to cmd either the new width
-            // or the new height
-
-            // if original width / original height is greater
-            // than new width / new height
-            if ($orig_w/$orig_h > $width/$height) {
-                // then resize to the new height...
-                $commands .= ' -resize "x'.$height.'"';
-
-                // ... and get the middle part of the new image
-                // what is the resized width?
-                $resized_w = ($height/$orig_h) * $orig_w;
-
-                // crop
-                $commands .= ' -crop "'.$width.'x'.$height.
-                    '+'.round(($resized_w - $width)/2).'+0"';
-            } else {
-                // or else resize to the new width
-                $commands .= ' -resize "'.$width.'"';
-
-                // ... and get the middle part of the new image
-                // what is the resized height?
-                $resized_h = ($width/$orig_w) * $orig_h;
-
-                // crop
-                $commands .= ' -crop "'.$width.'x'.$height.
-                    '+0+'.round(($resized_h - $height)/2).'"';
+            if (function_exists('part')) {
+                // crops the image to the requested size
+                if (!preg_match('/^[0-9]+x[0-9]+$/',$cmd[4])) {
+                    die('ERROR: Invalid parameter.');
+                }
+                list($width, $height) = explode('x', $cmd[4]);
+                $commands .= part($image, $width, $height);
             }
             break;
 
@@ -266,6 +268,7 @@ function dispatch($image, $cache, $cmds) {
 
     return $result;
 }
+
 
 /* 
  * serves an image 
@@ -330,135 +333,6 @@ function dprint($str, $print_r=false) {
             echo $str."<br/>\n";
         }
     }
-}
-
-/* 
- * edge-maximizing crop
- *
- * generates a thumbnail of desired dimensions from a source image by cropping 
- * the most "interesting" or edge-filled part of the image.
- *
- * $w, $h - target dimensions of thumbnail
- * $image - system path to source image
- * $out - path/name of output image
- */
-
-function opticrop($image, $w, $h, $out) {
-    // get size of the original
-    $imginfo = getimagesize($image);
-    $w0 = $imginfo[0];
-    $h0 = $imginfo[1];
-    if ($w > $w0 || $h > $h0)
-        die("Target dimensions must be smaller or equal to source dimensions.");
-
-    // parameters for the edge-maximizing crop algorithm
-    $r = 2;         // radius of edge filter
-    $nk = 3;        // scale count: number of crop sizes to try
-    $nx = 5;        // number of x-translations to try
-    $ny = 5;        // number of y-translations to try
-    $gamma = 0.8;   // edge-sum normalization parameter -- see documentation
-    $ar = $w/$h;    // target aspect ratio (AR)
-
-    dprint('$img: '.$image);
-    dprint('$w x $h: '.$w.'x'.$h);
-    dprint('$w0 x $h0: '.$w0.'x'.$h0);
-    $img = new Imagick($image);
-
-    // crop source img to target AR
-    if ($w0/$h0 > $ar) {
-        // source AR wider than target
-        // crop width to target AR
-        $wcrop0 = round($ar*$h0);
-        $hcrop0 = $h0;
-    } 
-    else {
-        // crop height to target AR
-        $wcrop0 = $w0;
-        $hcrop0 = round($w0/$ar);
-    }
-
-    // crop parameters for all scales and translations
-    $params = array();
-
-    // crop at different scales
-    $hgap = $hcrop0 - $h;
-    $hinc = $hgap / ($nk - 1);
-    for ($k = 0; $k < $nk; $k++) {
-        $hcrop = round($hcrop0 - $k*$hinc);
-        $wcrop = $hcrop*$ar;
-        // crop at different locations 
-        // space translations out evenly across source image
-        $xgap = $w0 - $wcrop;
-        $xinc = $xgap / $nx;
-        $ygap = $h0 - $hcrop;
-        $yinc = $ygap / $ny;
-
-        // crop is only slightly smaller than source
-        // proceed by 1px increments
-        $nxtemp = $nx;
-        $nytemp = $ny;
-        if ($xgap < $nx - 1) {
-            $nxtemp = $xgap + 1;
-            $xinc = 1;
-        }
-        if ($ygap < $ny - 1) {
-            $nytemp = $ygap + 1;
-            $yinc = 1;
-        }
-        // generate parameters for trial crops
-        for ($i=0; $i<$nxtemp; $i++) {
-            $xcrop = round($i * $xinc);
-            for ($j=0; $j<$nytemp; $j++) {
-                $ycrop = round($j * $yinc);
-                $params[] = array('wcrop'=>$wcrop, 'hcrop'=>$hcrop, 'xcrop'=>$xcrop, 'ycrop'=>$ycrop);
-            }
-        }
-    }
-    dprint("original:<br/><img src=\"".$_GET['src']."\"/>");
-    // crop each trial image, save the one with most edges
-    $i = 0;
-    $maximg = clone $img;
-    $maxbetanorm = 0;
-    $maxparam = "";
-    foreach ($params as $param) {
-        $i++;
-        $currfile = CACHE_PATH."image$i.jpg";
-        $beta = 0;
-        $currimg = clone $img;
-        $currimg->cropImage($param['wcrop'],$param['hcrop'],$param['xcrop'],$param['ycrop']);
-        $currimgcp = clone $currimg;        // save for output
-        $currimg->edgeImage($r);
-        $currimg->modulateImage(100,0,100); // grayscale
-        $currimg->writeImage($currfile);    // save for debug
-        $pi = $currimg->getPixelIterator();
-        foreach ($pi as $row=>$pixels) {
-            foreach($pixels as $column=>$pixel) {
-                $beta += $pixel->getColorValue(imagick::COLOR_RED);
-            }
-        }
-        $area = $param['wcrop'] * $param['hcrop'];
-        $betanorm = $beta / pow($area, $gamma);
-        dprint("$currfile (beta=$beta, normalized=$betanorm):<br/>\n<img src=\"$currfile\"/>");
-        dprint($param, true);
-        // best image found, save it
-        if ($betanorm > $maxbetanorm) {
-            $maxbetanorm = $betanorm;
-            $maxfile = $currfile;
-            $maximg->destroy();
-            $maximg = $currimgcp;
-        }
-        else {
-            $currimg->destroy();
-            $currimgcp->destroy();
-        }
-    }
-    $img->destroy();
-    $img = $maximg;
-    $img->scaleImage($w,$h);
-    $img->writeImage($out);
-    dprint("maxfile: $maxfile");
-
-    return 0;
 }
 
 ?>
