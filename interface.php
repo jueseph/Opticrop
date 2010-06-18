f3167efee5c921d0721b2ad6a0217c2d64b82433
U-AD\juewang <juewang@7746JK1.ad.aaas.org>
2010-06-18 17:09:10 -0400

    tweaks to interface

--- interface.php
+++ interface.php
@@ -3,6 +3,7 @@ define('DEBUG', 0);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+// debug printing
 function dprint($str) {
     echo $str."<br/>\n";
 }
@@ -10,6 +11,9 @@ function dprint($str) {
 $imgsrc = isset($_GET['img']) ? $_GET['img'] : 'test.jpg';
 $opticrop_url = "opticrop.php";
 $magick_url = "magick.php";
+$dims = array(  array('w'=>200, 'h'=>200),
+                array('w'=>170, 'h'=>40),
+                array('w'=>60, 'h'=>60));
 ?>
 
 <html>
@@ -27,9 +31,11 @@ $magick_url = "magick.php";
     </style>
 
     <script type="text/javascript"> 
-
-        // Remember to invoke within jQuery(window).load(...)
-        // If you don't, Jcrop may not initialize properly
+        // Don't use $(document).ready() -- that's bad for Jcrop b/c 
+        // it doesn't wait for images to load
+        //
+        // All Jcrop-related event-handling code should be in this block
+        // to access the Jcrop api variable
         $(window).load(function(){
             var fixTargetSize;
             var api;
@@ -48,26 +54,30 @@ $magick_url = "magick.php";
                 $('#h').val('100');
                 autoSetBounds();
 
+                // event handlers
+                $('#suggest').click(autoSetBounds);
+                $('#crop').click(doCrop);
+                $('#w, #h').change(changeSize);
+                $('#fixts').change(updateFixTargetSize);
+                
                 // display ajax errors
                 $('.log').ajaxError(function(e, xhr, settings, exception) {
                     $(this).text('Triggered ajaxError handler. URL:'+settings.url+' Response: '+xhr.statusText);
                 });
-                
-                // event handlers
-                $('#suggest').click(autoSetBounds);
-                $('#crop').click(validateAndSubmit);
-                $('#w, #h').change(changeSize);
-                $('#fixts').change(updateFixTargetSize);
+            }
+            
+            function log(str) {
+                $('#log').html(str);
             }
             
             function autoSetBounds() {            
-                $('.log').html('Computing crop...');
+                log('Computing crop...');
                 var url = "<?php echo $opticrop_url;?>";
                 $.get(url,
                     { src: "<?php echo $imgsrc;?>", format: 'json', w: $('#w').val(), h: $('#h').val() },
                     function(data, status, xhr) {
                         var jd = JSON.parse(data);
-                        $('.log').html('Request: '+url+'<br/>'+'Status: '+data);
+                        log('Request: '+url+'<br/>'+'Status: '+data);
                         api.setSelect([jd.x, jd.y, jd.x+jd.w, jd.y+jd.h]);
                     }
                 );
@@ -92,7 +102,7 @@ $magick_url = "magick.php";
                 var size = [c.x, c.y, c.x+parseFloat($('#w').val()), c.y+parseFloat($('#h').val())];
                 var str = "("+c.x+","+c.y+","+c.x2+","+c.y2+") =>"; 
                 str += "("+c.x+","+c.y+","+(c.x+parseFloat($('#w').val()))+","+(c.y+parseFloat($('#y').val()))+")";
-                $('.log').html(str);
+                log(str);
                 api.setOptions({aspectRatio: $('#w').val() / $('#h').val() });
                 api.setSelect(size);                     
                 api.focus();
@@ -112,7 +122,7 @@ $magick_url = "magick.php";
                 }
             };
             
-            function validateAndSubmit()
+            function doCrop()
             {
                 if (!parseInt($('#w').val())) {
                     alert('Please select a crop region then press submit.');
@@ -127,13 +137,13 @@ $magick_url = "magick.php";
                 qstr += "?src=<?php echo $imgsrc;?>";
                 qstr += "&cmd="+encodeURIComponent(cmd);
                 
-                $('.log').html('Building crop img tag...<br/>query: '+qstr);
+                log('Building crop img tag...<br/>query: '+qstr);
                 $('#output').html("<img src=\"http://localhost/opticrop/"+qstr+"\"/>");
                 /*var url = "<?php echo $magick_url;?>";
                 $.get(url,
                     { src: "<?php echo $imgsrc;?>", cmd: cmd},
                     function(data) {
-                        $('.log').html('request: '+url+'<br/>'+'Response: '+data);
+                        log('request: '+url+'<br/>'+'Response: '+data);
                     }
                 );*/
                 api.focus();
@@ -143,12 +153,17 @@ $magick_url = "magick.php";
 </head>
 <body>
     <h1>Automagic Crop Tool</h1>
-    <p>
+    <div id="image-queue">
         <a href="interface.php">Test 1</a> | 
         <a href="interface.php?img=test2.jpg">Test 2</a> | 
         <a href="interface.php?img=test3.jpg">Test 3</a> | 
         <a href="interface.php?img=test4.jpg">Test 4</a>
-    </p>
+    </div>
+    <ul id="preset-dims">
+<?php foreach ($dims as $dim) { ?>
+        <li class="preset-dim"><a id="dim<?php echo $dim['w']."x".$dim['h'];?>"><?php echo $dim['w']."x".$dim['h'];?></a></li>
+<?php }?>
+    </ul>
     <div class="log"></div>
 
     <img src="<?php echo $imgsrc;?>" id="cropbox"/>
